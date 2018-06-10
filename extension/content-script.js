e052df26eae5ba42ce412f22d833026889c3094b
zaimwa9 <B00549848@essec.edu>
2018-06-10 15:08:17 +0200

    extension finished

--- extension/content-script.js
+++ extension/content-script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     width: 100%; \
     text-align: center; \
     z-index: 99998; \
-    height: 50px; \
+    height: 60px; \
     background-color: ghostwhite; \
     padding-top: 8px;";
 
@@ -14,22 +14,31 @@ $(document).ready(function(){
     display: inline-block; \
     position: absolute; \
     text-align: center; \
-    right: 2%;\
-    top: 15%; \
+    right: 20px; \
+    top: 15px;\
     z-index: 99999; \
     height: 20px; \
     border: none; \
-    border-radius: 50%; \
-    background-color: #f7bec7; \
-    font-width: bold; \
+    background-color: #f912379c; \
+    font-weight: bold; \
+    font-size: 60%; \
     outline:none; \
+    padding: 4px 6px\
+    color: white;\
+    ";
+
+    var css3 = "\
+    display: inline-block; \
+    position: absolute; \
+    max-width: 20px; \
     ";
 
   $("html").prepend("<div id='ext' style='" + css + "'> \
   <p>\
     Vous voulez nous aider ? \
-    Confirmez qu'il s'agit d'un article de journalisme de solution et notez l'impact \
+    Confirmez qu'il s'agit d'un article de journalisme de solution et validez l'impact \
   </p>\
+  <img src='https://image.flaticon.com/icons/svg/535/535190.svg' style='" + css3 + "'></img>\
   <button id='close' style='" + css2 + "'>X</button>\
   </div>");
 
