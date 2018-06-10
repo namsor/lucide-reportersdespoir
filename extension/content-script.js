f51a6b72e95101b67d3ecb6afcc45f85fadf0e3c
zaimwa9 <B00549848@essec.edu>
2018-06-10 14:23:33 +0200

    closing tag extension

--- extension/content-script.js
+++ extension/content-script.js
@@ -5,12 +5,37 @@ $(document).ready(function(){
     position: absolute; \
     width: 100%; \
     text-align: center; \
-    z-index: 99999; \
+    z-index: 99998; \
     height: 50px; \
     background-color: ghostwhite; \
     padding-top: 8px;";
-  $("html").prepend("<div style='" + css + "'> \
-  <p>Vous voulez nous aider ? \
-  Confirmez qu'il s'agit d'un article de journalisme de solution et notez l'impact \
-  </p></div>");
+
+    var css2 = "\
+    display: inline-block; \
+    position: absolute; \
+    text-align: center; \
+    right: 2%;\
+    top: 15%; \
+    z-index: 99999; \
+    height: 20px; \
+    border: none; \
+    border-radius: 50%; \
+    background-color: #f7bec7; \
+    font-width: bold; \
+    outline:none; \
+    ";
+
+  $("html").prepend("<div id='ext' style='" + css + "'> \
+  <p>\
+    Vous voulez nous aider ? \
+    Confirmez qu'il s'agit d'un article de journalisme de solution et notez l'impact \
+  </p>\
+  <button id='close' style='" + css2 + "'>X</button>\
+  </div>");
+
+  $("#close").click(function() {
+    console.log('destroying')
+    $("#ext").remove();
+  });
 });
+
