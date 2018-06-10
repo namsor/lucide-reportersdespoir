8e5b9f53ab447bbfc3af8103f8df2ee13b207328
Florian MADURA <fmadura@student.42.fr>
2018-06-10 15:27:48 +0200

    free yourself

--- frontalgo/src/App.js
+++ frontalgo/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './header';
 import { Link } from 'react-router-dom'
 
 import {
@@ -69,37 +68,32 @@ class App extends Component {
     console.log('state coming')
     console.log(this.state.isSearching);
   }
-  // Author:Katia Barillot
-  // Source:Le Marais Mood
-  // Title:Journée du grand nettoyage
-  // Year:2017
-  // likelyGender:female
-  // rankingGendered:0.9683116233525264
-  // objectID:1476381941
-  // URL:http://www.lemaraismood.fr/journee-grand-nettoyage/
-  // Lieu:Le Marais
-  // Content:Chaque jour dans les rues de Paris 3,5 millions de personnes se déplacent, forcément cela entraine une certaine quantité de saleté. 30 000 corbeilles de rues sont collectées et 234 tournées de ramassage d’ordures ménagères sont effectuées. C’est dire s’il y a à faire. C’est pourquoi tous les ans, fin septembre, pour sensibiliser les parisiens, la capitale organise la Journée du Grand Nettoyage. Le 30 septembre, coachés par les cantonniers, vous pourrez donner un coup de balai et enrayer la saleté qui a envahi nos rues. Car ces poubelles qui débordent, ces détritus jetés à même le sol font très mauvais effet dans l’une des villes les plus visitées au monde. Benjamin Djiane, l’Adjoint au Maire du 3ème arrondissement chargé de la démocratie locale, de la sécurité, de la prévention, de la propreté et de la prévention des nuisances nous explique par quelle action citoyenne des maraisiens vont redonner à nos rues une apparence impeccable.
-  // MainPIC:http://www.lemaraismood.fr/wp-content/uploads/2017/09/DSC09154-960x500.jpg
-  // Date:29/09/2017
-  // genderScale:1
-  // solutionRating:0.404385147012253
-  // problemRating:0.4229664595374781
-  // impactJournalismScore:0.9683116233525264
-
 
   render() {
     return (
       <div className="App">
-      <Header />
-        <InstantSearch
-          appId="89X1HYV9FR"
-          apiKey="50b8887a34b8f6c7420ffe27fbb855e2"
-          indexName="reportersdespoirs"
-        >
+      <div className='MyHeader'>
+        <header className="App-header">
+          <h1 className="App-title">Search Positive</h1>
+        </header>
+        <div className='links'>
+          <Link to='/analytics'>Analytic search</Link>
+        </div>
+      </div>
+      <InstantSearch
+      appId="89X1HYV9FR"
+      apiKey="50b8887a34b8f6c7420ffe27fbb855e2"
+      indexName="reportersdespoirs"
+      >
           <ConnectedSearchBox setter={this.setter} />
-          {(this.state.isSearching) ? <RefinementList attribute="Year" /> : ''}
-          {(this.state.isSearching) ? <RefinementList attribute="likelyGender" /> : ''}
-          {(this.state.isSearching) ? <RefinementList attribute="Source" /> : ''}
+          <div className="ais-refinebox">
+            {(this.state.isSearching) ? <div className='refine-title' >Année</div>: ''}
+            {(this.state.isSearching) ? <RefinementList attribute="Year" /> : ''}
+            {(this.state.isSearching) ? <div className='refine-title'>Genre</div>: ''}
+            {(this.state.isSearching) ? <RefinementList attribute="likelyGender" /> : ''}
+            {(this.state.isSearching) ? <div className='refine-title'>Source</div>: ''}
+            {(this.state.isSearching) ? <RefinementList attribute="Source" /> : ''}
+          </div>
           {(this.state.isSearching) ? <Hits hitComponent={Product}/> : ''}
         </InstantSearch>
       </div>
